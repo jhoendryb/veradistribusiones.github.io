eae47f22abe4235deacee824a490c14d07722a32
jhoendryb <jhoendryblanco@hotmail.com>
2017-09-04 10:48:52 -0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     
     $(function(){
-        $('#lider div:gt(0)').hide();
+        $('#slider div:gt(0)').hide();
         setInterval(function(){
           $('#slider div:first-child').fadeOut(0)
              .next('div').fadeIn(1000)
